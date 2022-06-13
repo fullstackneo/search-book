6db036245f28151daeaaa40126a08743f2a1b264
Neo <fullstackneo@gmail.com>
2022-06-13 03:03:37 -0600

    add Apollo Porvider to connect apollo server

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,53 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import {
+  ApolloProvider,
+  ApolloClient,
+  InMemoryCache,
+  createHttpLink,
+} from '@apollo/client';
 import SearchBooks from './pages/SearchBooks';
 import SavedBooks from './pages/SavedBooks';
 import Navbar from './components/Navbar';
+import { setContext } from '@apollo/client/link/context';
+
+const httpLink = createHttpLink({
+  // establish a new link to the GraphQL server at its /graphql endpoint with createHttpLink()
+  uri: '/graphql',
+});
+
+const authLink = setContext((_, { headers }) => {
+  // we use the setContext() function to retrieve the token from localStorage
+  const token = localStorage.getItem('id_token');
+  return {
+    // set the HTTP request headers of every request to include the token, whether the request needs it or no
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  };
+});
+
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  // InMemoryCache enables the Apollo Client instance to cache API response data so that we can perform requests more efficiently.
+  cache: new InMemoryCache(),
+});
 
 function App() {
   return (
-    <Router>
-      <>
-        <Navbar />
-        <Switch>
-          <Route exact path='/' component={SearchBooks} />
-          <Route exact path='/saved' component={SavedBooks} />
-          <Route render={() => <h1 className='display-2'>Wrong page!</h1>} />
-        </Switch>
-      </>
-    </Router>
+    <ApolloProvider client={client}>
+      <Router>
+        <>
+          <Navbar />
+          <Switch>
+            <Route exact path="/" component={SearchBooks} />
+            <Route exact path="/saved" component={SavedBooks} />
+            <Route render={() => <h1 className="display-2">Wrong page!</h1>} />
+          </Switch>
+        </>
+      </Router>
+    </ApolloProvider>
   );
 }
 
